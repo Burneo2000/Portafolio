338ffc5340d3373b17134895c36b42521e09ba1c
Burneo <burneocristian5@gmail.com>
2021-06-18 10:51:00 -0500

    Se corrigio el error en la barra de navegación y se optimizo las animaciones

--- js/indexAnimaciones.js
+++ js/indexAnimaciones.js
@@ -11,10 +11,10 @@ const animar = (clase, duracion, origen, distancia) => {
 animar('.animarMenu',3000,'bottom','-100px');
 animar('.container__presentacion',3000,'top','100px');
 animar('.container__presentacion-texto',4000,'top','100px');
-animar('.container__redes',4000,'right','100px');
-animar('.container__imagen',6000,'right','100px');
+animar('.container__redes',4000,'right','80px');
+animar('.container__imagen',6000,'right','80px');
 animar('.animarLogo',3000,'bottom','-100px');
-animar('.sobreMi',3000,'right','400px');
+animar('.sobreMi',3000,'right','80px');
 
 // Mis habilidades
 animar('.misHabilidades',3000, 'top','100px');
